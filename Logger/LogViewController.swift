a52f857f427489bac726b24830a52508b2bbdabf
James Little <littleguy23@gmail.com>
2016-11-11 17:05:08 -0500

    Start trying to connect to a robot with a socket

--- Logger/LogViewController.swift
+++ Logger/LogViewController.swift
@@ -15,6 +15,9 @@ class LogViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = robot!.prettyName
+        
+        let conn = Connection()
+        conn.connect(host: "www.example.com", port: 80)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,4 +32,66 @@ class LogViewController: UIViewController {
         
     }
 
+}
+
+class Connection: NSObject, StreamDelegate {
+    var inputStream: InputStream?
+    var outputStream: OutputStream?
+    
+    func connect(host: String, port: Int) {
+        Stream.getStreamsToHost(withName: "127.0.0.1", port: 30000, inputStream: &inputStream, outputStream: &outputStream)
+        
+        if inputStream != nil && outputStream != nil {
+            // Set delegate
+            inputStream!.delegate = self
+            outputStream!.delegate = self
+            
+            // Schedule
+            inputStream!.schedule(in: .main, forMode: RunLoopMode.defaultRunLoopMode)
+            outputStream!.schedule(in: .main, forMode: RunLoopMode.defaultRunLoopMode)
+            
+            print("Start open()")
+            
+            // Open!
+            inputStream!.open()
+            outputStream!.open()
+            
+            print(inputStream!.hasBytesAvailable)
+//            outputStream?.write("H", maxLength: 1)
+        }
+    }
+    
+    func stream(aStream: Stream, handleEvent eventCode: Stream.Event) {
+        print("stream")
+//        if aStream === inputStream {
+//            switch eventCode {
+//            case Stream.Event.errorOccurred:
+//                print("input: ErrorOccurred: \(aStream.streamError)")
+//            case Stream.Event.openCompleted:
+//                print("input: OpenCompleted")
+//            case Stream.Event.hasBytesAvailable:
+//                print("input: HasBytesAvailable")
+//                
+//                // Here you can `read()` from `inputStream`
+//                
+//            default:
+//                break
+//            }
+//        }
+//        else if aStream === outputStream {
+//            switch eventCode {
+//            case Stream.Event.errorOccurred:
+//                print("output: ErrorOccurred: \(aStream.streamError)")
+//            case Stream.Event.openCompleted:
+//                print("output: OpenCompleted")
+//            case Stream.Event.hasSpaceAvailable:
+//                print("output: HasSpaceAvailable")
+//                
+//                // Here you can write() to `outputStream`
+//                
+//            default:
+//                break
+//            }
+//        }
+    }
 }
